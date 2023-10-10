b4dc5923e9ecdc07897bff2bef277aa760f3af1b
Pratheesha97 <samadhipratheesha@gmail.com>
2023-10-10 22:15:55 +0530

    Add Sample SOAP Service Impl

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const dotenv = require('dotenv');
+const soap = require('soap');
+const fs = require('fs');
 
 // Load environment variables from a .env file if it exists
 dotenv.config();
@@ -33,7 +35,39 @@ app.post('/api/postData', (req, res) => {
   res.json(requestData);
 });
 
+
+// Sample SOAP service implementation
+const service = {
+  WeatherService: {
+    WeatherPort: {
+      GetWeather: function(args, callback) {
+        // Perform the logic to get the weather based on the location
+        const location = args.location;
+        const temperature = '25°C';
+        const description = 'Sunny';
+
+        // Return the response
+        const result = {
+          temperature: temperature,
+          description: description
+        };
+        callback(null, result);
+      }
+    }
+  }
+};
+
+// Read the WSDL file as an XML string
+const wsdlXML = fs.readFileSync('wsdl/weatherService.wsdl', 'utf8');
+
+// Create the SOAP server
+soap.listen(app, '/weather', service, wsdlXML, function() {
+  console.log('SOAP server running at http://localhost:8000/weather?wsdl');
+});
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
